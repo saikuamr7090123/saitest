c78d4fbe5ab752be6eed8f3d879093c1b4b0c46e
harithota2112@gmail.com <harithota2112@gmail.com>
2023-06-08 17:51:08 +0000

    x

--- models/example/testcase.sql
+++ models/example/testcase.sql
@@ -1,8 +1,8 @@
 
-{ config(materialized='table') }}
+{{ config(materialized='table') }}
 
 with sde as(
-select *, case when deptid= 10 then 30000 when deptid  = 20 then 50000 else 70000 end as salary
+select *, case when deptid= 10 then 30000 when deptid  = 20 then 50000 else 70000 end as salarys
   from emp
 )
 select * from sde
